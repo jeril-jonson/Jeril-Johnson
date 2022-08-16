bac8f6014b208580e32087af76c1315da20fffcf
jeril-jonson <111279249+jeril-jonson@users.noreply.github.com>
2022-08-16 12:21:37 +0530

    Create adding elements in a array.c

---
+++ adding elements in a array.c
@@ -0,0 +1,16 @@
+#invlude<stdio.h>
+int main()
+{
+int i,a[20],n,sum=0;
+printf("enter the number of elements");
+scanf("%d",&n);
+printf("enter n elements");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+ sum=sum+a[i];
+}
+  printf("the required sum is \n%d",sum);
+  return 0;
+  }
+  
